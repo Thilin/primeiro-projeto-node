95c6c61e75d90035106e4bec6ed6bba1a07bd574
Thilin <fernando_cauper@hotmail.com>
2021-01-07 13:41:36 -0400

    refactor: method get and save to use the database

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,28 +1,25 @@
 import Appointment from "../models/Appointment";
 import {startOfHour} from "date-fns";
 import AppointmentsRepository from "../repositories/AppointmentsRepository";
+import {getCustomRepository} from "typeorm";
 
 interface Request{
     provider: string;
     date: Date;
 }
 class CreateAppointmentService{
-    private appointmentsRepository: AppointmentsRepository;
 
-    constructor(appointmentRepository: AppointmentsRepository) {
-        this.appointmentsRepository = appointmentRepository;
-    }
-
-    public execute({date, provider}: Request): Appointment{
+    public async execute({date, provider}: Request): Promise<Appointment>{
+        const appointmentsRepository = getCustomRepository(AppointmentsRepository);
         const appointmentDate = startOfHour(date);
-        const findAppointmentInSameDate = this.appointmentsRepository.findByDate(appointmentDate);
+        const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate);
 
         if(findAppointmentInSameDate) {
             throw Error('This appointment is already booked');
         }
 
-        const appointment = this.appointmentsRepository.create({provider, date: appointmentDate});
-
+        const appointment = appointmentsRepository.create({provider, date: appointmentDate});
+        await appointmentsRepository.save(appointment);
         return appointment;
     }
 }
