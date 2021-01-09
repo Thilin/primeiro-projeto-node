09901e6b75e247b74e8229aeeec9ff17a7ece2c8
Thilin <fernando_cauper@hotmail.com>
2021-01-09 10:33:27 -0400

    add: now its possible to save a user avatar

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,7 +1,11 @@
 import {Router} from "express";
 import CreateUserService from "../services/CreateUserService";
+import ensureAuthenticated from "../middlewares/EnsureAuthenticated";
+import multer from "multer";
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) =>{
     try{
@@ -14,7 +18,11 @@ usersRouter.post('/', async (request, response) =>{
     }catch(err){
         return response.status(400).json({error: err.message});
     }
-})
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) =>{
+   return response.json({ok: true});
+});
 
 
 export default usersRouter;
