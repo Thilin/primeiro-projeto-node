b32794301ac544596b91630dae4b77b45fe24927
Thilin <fernando_cauper@hotmail.com>
2021-01-09 17:21:30 -0400

    refactor: separeted the logic to controllers instead the routes

--- src/modules/users/infra/http/routes/users.route.ts
+++ src/modules/users/infra/http/routes/users.route.ts
@@ -1,36 +1,18 @@
 import {Router} from "express";
-import {container} from "tsyringe";
-import CreateUserService from "@modules/users/services/CreateUserService";
-import UpdateUserAvatarService from "@modules/users/services/UpdateUserAvatarService";
-import ensureAuthenticated from "@modules/users/infra/http/middlewares/EnsureAuthenticated";
 import multer from "multer";
 import uploadConfig from '@config/upload';
+import UsersController from "@modules/users/infra/http/controllers/UsersController";
+import UserAvatarController from "@modules/users/infra/http/controllers/UserAvatarController";
 
 const usersRouter = Router();
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 const upload = multer(uploadConfig);
 
 
-usersRouter.post('/', async (request, response) =>{
-    const {name, email, password} = request.body;
-    const createUser = container.resolve(CreateUserService);
-    const user = await createUser.execute({name, email, password});
-    delete user.password;
+usersRouter.post('/', usersController.create);
 
-    return response.json(user);
-});
-
-usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) =>{
-    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-
-   const user = await updateUserAvatar.execute({
-       user_id: request.user.id,
-       avatarFileName: request.file.filename
-   });
-
-   delete user.password;
-
-   return response.json(user);
-});
+usersRouter.patch('/avatar', userAvatarController.update);
 
 
 export default usersRouter;
