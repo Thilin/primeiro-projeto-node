b5a1cd70b6b562d2853f11f86e364c13bd2d02c6
Thilin <fernando_cauper@hotmail.com>
2021-01-09 21:01:17 -0400

    test: ensure to not create two appointments on the same hour

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import CreateAppointmentService from "@modules/appointments/services/CreateAppointmentService";
 import FakeAppointmentsRepository from "@modules/appointments/repositories/fakes/FakeAppointmentsRepository";
+import AppError from "@shared/errors/AppError";
 
 describe('CreateAppointment', () =>{
     it('should be able to create a new appointment', async () => {
@@ -13,7 +14,13 @@ describe('CreateAppointment', () =>{
         expect(appointment.provider_id).toBe('12312');
     });
 
-    it('should not be able to create two appointments on the same time', () => {
+    it('should not be able to create two appointments on the same time', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+        const appointmentDate = new Date(2021, 4, 10, 11);
+        const appointment = await createAppointment.execute({date: appointmentDate, provider_id: '12312'});
+
+        expect(createAppointment.execute({date: appointmentDate, provider_id: '12312'})).rejects.toBeInstanceOf(AppError);
 
     });
 })
